11d210e41ed7039a11fb0556e51da339371637ae
Erin Fahy <efahy@stanford.edu>
2014-01-15 14:08:39 -0800

    add in mass_assignment_sanitizer conf in staging env

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -68,4 +68,7 @@ D2g::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Raise exception on mass assignment protection for Active Record models
+  config.active_record.mass_assignment_sanitizer = :strict
 end
