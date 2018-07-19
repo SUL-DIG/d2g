15a7ccc4301b4758e406b52ff929db6e98bb918c
Chris Beer <cabeer@stanford.edu>
2018-07-19 09:18:27 -0700

    Update rails

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'rails', '4.0.11'
+gem 'rails', '~> 4.2'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 
   attr_accessible :email, :password, :password_confirmation if Rails::VERSION::MAJOR < 4
-# Connects this user object to Blacklights Bookmarks. 
+# Connects this user object to Blacklights Bookmarks.
   include Blacklight::User
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
 
   # Method added by Blacklight; Blacklight uses #to_s on your
   # user class to get a user-displayable login/identifier for
-  # the account. 
+  # the account.
   def to_s
     email
   end
